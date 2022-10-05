503e9685d335b5d9e9b77a45ef3f73a7d40c2be0
Shobhit Singh Pal <65528935+Shobhit-creator@users.noreply.github.com>
2022-10-05 12:23:34 +0530

    Update type of categories

--- Params/QueryParameters.cs
+++ Params/QueryParameters.cs
@@ -4,8 +4,8 @@ public class DefaultParameters{
     public int limit {get; set;}
 }
 public class QueryParameters:DefaultParameters{
-    public string[]? brands {get; set;} 
-    public string[]? categories {get; set;}
+    public string? brands {get; set;} 
+    public string? categories {get; set;}
     public int min_price {get; set;}
     public int max_price {get; set;}
 }
