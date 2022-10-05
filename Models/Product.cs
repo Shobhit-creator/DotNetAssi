80ed5679347e76a69c58d95d170201ac5f812b00
Shobhit Singh Pal <65528935+Shobhit-creator@users.noreply.github.com>
2022-10-05 23:04:42 +0530

    Update Product.cs
    update description name to PDescription

--- Models/Product.cs
+++ Models/Product.cs
@@ -4,7 +4,7 @@ public class Product{
     public int id {get; set; }
     public string title {get; set;}
 
-    public string description {get; set;}
+    public string PDescription {get; set;}
 
     public int price {get; set;}
 
