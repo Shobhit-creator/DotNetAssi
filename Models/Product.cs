844e4de66614c7940f07caf29be0aaa1fa44ac1c
Shobhit Singh Pal <65528935+Shobhit-creator@users.noreply.github.com>
2022-10-12 18:22:24 +0530

    Update Product.cs

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,14 +1,19 @@
 namespace WebApi.Models;
 
-public class Product{
+
+public class Image{
+    public string? images {get; set;}
+}
+
+public class Product:Image{
     public int id {get; set; }
     public string title {get; set;}
 
-    public string PDescription {get; set;}
+    public string description {get; set;}
 
     public int price {get; set;}
 
-    public float Discount {get; set;}
+    public float discount {get; set;}
 
     public float rating {get; set;}
 
@@ -19,12 +24,6 @@ public class Product{
     public string category {get; set;}
 
     public string thumbnail {get; set;}
-
-    public string image1 {get; set;}
-    public string image2 {get; set;}
-    public string image3 {get; set;}
-    public string image4 {get; set;}
-    public string image5 {get; set;}
-    public string image6 {get; set;}
-
+    
 }
+
