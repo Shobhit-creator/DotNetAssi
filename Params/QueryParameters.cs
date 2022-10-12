14c5e72e3d5de1769395cd818f3a946a4f808d85
Shobhit Singh Pal <65528935+Shobhit-creator@users.noreply.github.com>
2022-10-12 18:23:34 +0530

    Update QueryParameters.cs

--- Params/QueryParameters.cs
+++ Params/QueryParameters.cs
@@ -1,11 +1,13 @@
 namespace WebApi.Params;
 
 public class DefaultParameters{
-    public int limit {get; set;}
+    public int offset {get; set;}
 }
+
 public class QueryParameters:DefaultParameters{
-    public string? brands {get; set;} 
+    public string? brands {get; set;}
     public string? categories {get; set;}
     public int min_price {get; set;}
+
     public int max_price {get; set;}
 }
